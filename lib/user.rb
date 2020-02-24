98e53d707173b92a6fff70231aa0f575d9150163
liatmoss <liatmoss@hotmail.com>
2020-02-24 22:53:35 +0000

    tests passing for authenticate user login

--- lib/user.rb
+++ lib/user.rb
@@ -18,5 +18,17 @@ class User
     User.new(id: result[0]['id'], fullname: result[0]['fullname'], email: result[0]['email'], password: result[0]['password'])
   end
 
+  def self.where(id:)
+    DbConnection.setup(ENV["DB_NAME"])
+    result = DbConnection.query("SELECT * FROM GOLDENSHOE_USER WHERE id = #{id};")
+    User.new(id: result[0]['id'], fullname: result[0]['fullname'], email: result[0]['email'], password: result[0]['password'])
+  end
+
+  def self.authenticate(email:, password:)
+   DbConnection.setup(ENV["DB_NAME"])
+   result = DbConnection.query("SELECT * FROM GOLDENSHOE_USER WHERE email = '#{email}' AND password = '#{password}';")
+   return unless result.any?
+   User.new(id: result[0]['id'], fullname: result[0]['fullname'], email: result[0]['email'], password: result[0]['password'])
+ end
 
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,8 +4,25 @@ describe User do
 
   describe("add") do
     it "adds a single user" do
-      user = User.add(fullname: "Liat Moss", email: "liat@test.com",password:"123456")
+      user = User.add(fullname: "Liat Moss", email: "liat@test.com", password:"123456")
       expect(user.email).to eq "liat@test.com"
+    end
+  end
+
+  describe "select a user" do
+    it "returns 1 user" do
+      returnedUser = User.add(fullname: "Liat Moss", email: "liat@test.com", password:"123456")
+      user = User.where(id: returnedUser.id)
+      expect(user.id).to eq(returnedUser.id)
+    end
+  end
+
+  describe 'authenticate' do
+    it 'user can authenticate' do
+      returnedUser = User.add(fullname: "Liat Moss", email: "liat@test.com", password:"123456")
+      user = User.authenticate(email: "liat@test.com", password:"123456")
+      expect(user.email).to eq "liat@test.com"
+      expect(user.password).to eq "123456"
     end
   end
 end
