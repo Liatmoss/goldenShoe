fa4fd6f0c7e92e56f882f22814d7703f542063dc
liatmoss <liatmoss@hotmail.com>
2020-02-25 13:19:01 +0000

    added error for invalid password

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -10,4 +10,13 @@ feature 'User can login' do
     click_button("Submit")
     expect(page).to have_link "Sign out"
   end
+
+  scenario 'Invalid email address' do
+    visit ('/login')
+    fill_in("email", with: "test@test.com")
+    fill_in("password", with: "111111")
+    click_button("Submit")
+    expect(page).to have_content "EMAIL OR PASSWORD NOT VALID"
+  end
+
 end
