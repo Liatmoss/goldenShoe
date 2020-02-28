d3f65759634bddfb6bcf57765b872730fadc2eac
liatmoss <liatmoss@hotmail.com>
2020-02-28 15:06:53 +0000

    passing tests for shoe class

--- spec/shoe_spec.rb
+++ spec/shoe_spec.rb
@@ -15,10 +15,24 @@ describe Shoe do
   end
 
   describe "Out of stock" do
-  it "returns out of stock shoes" do
-    out_of_stock = Shoe.where(shoe_name: 'heel1', shoe_stock: 0)
-    expect(out_of_stock.length).to eq (2)
+    it "returns out of stock shoes" do
+      out_of_stock = Shoe.where(shoe_name: 'heel1', shoe_stock: 0)
+      expect(out_of_stock.length).to eq (2)
+    end
   end
-end
+
+  describe "select specific shoe size" do
+    it "finds the id of a shoe in a specific size" do
+      shoes = Shoe.select(shoe_size: 7)
+      expect(shoes).to eq 4
+    end
+  end
+
+  describe "finds specific users shoe from order" do
+    it "finds a shoe based on id" do
+      shoes = Shoe.shoe_order(id: 1)
+      expect(shoes[0].shoe_size).to eq("4")
+    end
+  end
 
 end
